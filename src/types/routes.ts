0bf993e9e4f11d9fc55a31c01636ef3a0c8da044
wangyingjie123 <2969057196@qq.com>
2023-05-31 07:05:08 +0000

    Deploying to publish from @ wangyingjie123/vue3-model@a5284a8253058750d39a27069dce39053100ec1f 🚀

--- src/types/routes.ts
+++
@@ -1,6 +0,0 @@
-import type { RouteRecordRaw } from 'vue-router';
-export type RouteItem = RouteRecordRaw & {
-  title?: string;
-  hidden?: boolean;
-  children?: RouteItem[];
-};
--- src/utils/copy.ts
+++
@@ -1,9 +0,0 @@
-import { ElMessage } from 'element-plus';
-export const copy = async (text: string) => {
-  try {
-    await navigator.clipboard.writeText(text);
-    ElMessage.success('Page URL copied to clipboard');
-  } catch (err) {
-    console.error('Failed to copy: ', err);
-  }
-};
