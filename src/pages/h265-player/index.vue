96158acf00a598c2607bbb14ab225a5a40f650a5
wangyingjie <wangyingjie@piesat.cn>
2023-05-31 17:19:56 +0800

    fix: 修改静态资源路径

--- src/pages/h265-player/index.vue
+++ src/pages/h265-player/index.vue
@@ -8,7 +8,7 @@ import GreenPlayer from 'goldvideo-player';
 import 'goldvideo-player/dist/goldplay-h265.css';
 
 onMounted(() => {
-  const libPath = import.meta.env.MODE === 'development' ? '/js/h265-wasm/' : '/vue3-module/js/h265-wasm/';
+  const libPath = import.meta.env.MODE === 'development' ? '/js/h265-wasm/' : '/vue3-model/js/h265-wasm/';
   new GreenPlayer(document.querySelector('#video'), {
     sourceURL: 'https://omc3i.codesandbox.io/ts/playlist.m3u8',
     libPath: `${window.location.origin}${libPath}`,
