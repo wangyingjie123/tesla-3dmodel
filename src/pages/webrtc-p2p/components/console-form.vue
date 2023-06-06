10934be1166dc43cad84e002071bf67fc05b1849
wangyingjie <wangyingjie@piesat.cn>
2023-06-06 15:48:49 +0800

    feat: 发送指令

---
+++ src/pages/webrtc-p2p/components/console-form.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-card class="box-card">
+    <label class="operation-label">指令：</label>
+    <el-input
+      v-model="instruction"
+      style="width: 200px; margin-right: 20px"
+      placeholder="要发送的指令"
+      clearable
+      :disabled="btnDiabled"
+      @keyup.enter="sendInstruction"
+    ></el-input>
+    <el-button type="primary" @click="sendInstruction" :disabled="btnDiabled">发送指令</el-button>
+    <el-button :type="isAudioOpen ? 'warning' : 'primary'" @click="handleAudio" :disabled="btnDiabled">
+      {{ isAudioOpen ? '关闭' : '打开' }}麦克风
+    </el-button>
+    <el-button :type="isCameraOpen ? 'warning' : 'primary'" @click="handleCamera" :disabled="btnDiabled"
+      >{{ isCameraOpen ? '关闭' : '打开' }}视频</el-button
+    >
+    <el-button type="danger" @click="handleLeave" :disabled="btnDiabled">关闭通信</el-button>
+    <el-popover placement="bottom" title="指令面板" :width="500" trigger="click">
+      <el-input v-model="textarea" readonly placeholder="Please input" :rows="6" show-word-limit type="textarea" />
+      <template #reference>
+        <el-button :disabled="btnDiabled">打开对话框</el-button>
+      </template>
+    </el-popover>
+  </el-card>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const props = defineProps({
+  localStream: {
+    type: MediaStream,
+    required: true,
+  },
+  dataChannel: {
+    type: RTCDataChannel,
+    required: true,
+  },
+  peerConnection: {
+    type: RTCPeerConnection,
+    required: true,
+  },
+  btnDiabled: {
+    type: Boolean,
+    required: true,
+  },
+});
+const instruction = ref('');
+const textarea = ref('');
+const isAudioOpen = ref(false);
+const isCameraOpen = ref(true);
+
+const writeInfo = (info: string) => {
+  textarea.value += `${info}\n`;
+};
+const sendInstruction = () => {
+  props.dataChannel.send(instruction.value);
+  writeInfo(`发送指令：${instruction.value}`);
+  instruction.value = '';
+};
+// 关闭连接
+const handleLeave = () => {
+  props.peerConnection.close();
+};
+// 打开/关闭麦克风
+const handleAudio = () => {
+  isAudioOpen.value = !isAudioOpen.value;
+  props.localStream.getAudioTracks().forEach((track) => {
+    track.enabled = isAudioOpen.value;
+  });
+};
+// 打开/关闭摄像头
+const handleCamera = () => {
+  isCameraOpen.value = !isCameraOpen.value;
+  props.localStream.getVideoTracks().forEach((track) => {
+    track.enabled = isCameraOpen.value;
+  });
+};
+// 将这个方法暴露出去,这样父组件就可以使用了哈
+defineExpose({
+  writeInfo,
+});
+</script>
+<style lang="scss" scoped></style>
--- src/pages/webrtc-p2p/index.vue
+++ src/pages/webrtc-p2p/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-container>
+  <el-container class="p2p-main">
     <el-header class="p2p-header">
       <el-text type="warning" size="large">Webrtc-P2P通话</el-text>
       <el-button @click="drawer = true" type="primary">打开操作面板</el-button>
@@ -17,7 +17,15 @@
         </el-col>
       </el-row>
     </el-main>
-    <el-footer></el-footer>
+    <el-footer>
+      <ConsoleForm
+        :localStream="localStream"
+        :peerConnection="peerConnection"
+        :dataChannel="dataChannel"
+        :btnDiabled="btnDiabled"
+        ref="consoleRef"
+      ></ConsoleForm>
+    </el-footer>
   </el-container>
   <el-drawer v-model="drawer" title="本地操作区域-打开2个tab页">
     <OfferForm
@@ -32,20 +40,28 @@
 <script setup lang="ts">
 import 'webrtc-adapter';
 import { ref, onMounted } from 'vue';
-import OfferForm from './offer-form.vue';
+import { ElMessage } from 'element-plus';
+import OfferForm from './components/offer-form.vue';
+import ConsoleForm from './components/console-form.vue';
 const localRef = ref<HTMLVideoElement>();
 const remoteRef = ref<HTMLVideoElement>();
 // 创建本地/远程 SDP 描述, 用于描述本地/远程的媒体流
+const localStream = ref<MediaStream>(new MediaStream());
+const peerConnection = ref<RTCPeerConnection>(
+  new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: 'stun:172.16.40.202:3478',
+      },
+    ],
+  })
+);
+const dataChannel = ref<RTCDataChannel>(peerConnection.value.createDataChannel('message'));
+const consoleRef = ref<{ writeInfo: (info: string) => void }>();
 const localOffer = ref('');
 const remoteAnswer = ref('');
 const drawer = ref(false);
-const pc = new RTCPeerConnection({
-  iceServers: [
-    {
-      urls: 'stun:stun.voipbuster.com',
-    },
-  ],
-});
+const btnDiabled = ref(true);
 // 初始化
 const init = async () => {
   // 获取本地端视频标签
@@ -53,67 +69,82 @@ const init = async () => {
   // 获取远程端视频标签
   const remoteVideo = remoteRef.value!;
   // 采集本地媒体流
-  const localStream = await navigator.mediaDevices.getUserMedia({
+  localStream.value = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
   });
   // 设置本地视频流
-  localVideo.srcObject = localStream;
-  // 不推荐使用：已经过时的方法 [addStream API](https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addStream)
-  // pc.addStream(localStream);
+  localVideo.srcObject = localStream.value;
   // 添加本地媒体流的轨道都添加到 RTCPeerConnection 中
-  localStream.getTracks().forEach((track) => {
-    pc.addTrack(track, localStream);
+  localStream.value.getTracks().forEach((track) => {
+    peerConnection.value.addTrack(track, localStream.value);
   });
-  // 监听远程流，方法一：
-  pc.ontrack = (event) => {
-    remoteVideo.srcObject = event.streams[0];
+  // 先禁用音频
+  localStream.value.getAudioTracks().forEach((track) => {
+    track.enabled = true;
+  });
+  // 监听文本信息
+  peerConnection.value.ondatachannel = function (event) {
+    ElMessage.success('建立连接');
+    btnDiabled.value = false;
+    const channel = event.channel;
+    channel.onopen = function () {
+      channel.send('建立连接!');
+    };
+    channel.onmessage = (event) => {
+      console.log(event.data);
+      consoleRef.value?.writeInfo(`接收到的指令：${event.data}`);
+    };
   };
+  // 监听远程流，方法一：
+  // peerConnection.value.ontrack = (event) => {
+  //   remoteVideo.srcObject = event.streams[0];
+  // };
   // 方法二：你也可以使用 addStream API，来更加详细的控制流的添加
-  // const remoteStream: MediaStream = new MediaStream()
-  // pc.ontrack = (event) => {
-  //   event.streams[0].getTracks().forEach((track) => {
-  //     remoteStream.addTrack(track)
-  //   })
-  //   // 设置远程视频流
-  //   remoteVideo.srcObject = remoteStream
-  // }
+  const remoteStream: MediaStream = new MediaStream();
+  peerConnection.value.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+    // 设置远程视频流
+    remoteVideo.srcObject = remoteStream;
+  };
 };
 const createOffer = async () => {
   // 创建 offer
-  const offer = await pc.createOffer();
+  const offer = await peerConnection.value.createOffer();
   // 设置本地描述
-  await pc.setLocalDescription(offer);
+  await peerConnection.value.setLocalDescription(offer);
   // 到这里，我们本地的 offer 就创建好了，一般在这里通过信令服务器发送 offerSdp 给远端
   // 监听 RTCPeerConnection 的 onicecandidate 事件，当 ICE 服务器返回一个新的候选地址时，就会触发该事件
-  pc.onicecandidate = async (event) => {
+  peerConnection.value.onicecandidate = async (event) => {
     if (!event.candidate) {
       return;
     }
-    localOffer.value = JSON.stringify(pc.localDescription);
+    localOffer.value = JSON.stringify(peerConnection.value.localDescription);
   };
 };
 const createAnswer = async (localOffer: string) => {
   if (!localOffer) return;
   // 解析字符串
   const offer = JSON.parse(localOffer);
-  pc.onicecandidate = async (event) => {
+  peerConnection.value.onicecandidate = async (event) => {
     // Event that fires off when a new answer ICE candidate is created
     if (event.candidate) {
-      const answer = JSON.stringify(pc.localDescription);
+      const answer = JSON.stringify(peerConnection.value.localDescription);
       remoteAnswer.value = answer;
     }
   };
-  await pc.setRemoteDescription(offer);
-  const answer = await pc.createAnswer();
-  await pc.setLocalDescription(answer);
+  await peerConnection.value.setRemoteDescription(offer);
+  const answer = await peerConnection.value.createAnswer();
+  await peerConnection.value.setLocalDescription(answer);
 };
 // 添加 answer(应答)
 const addAnswer = async (remoteAnswer: string) => {
   if (!remoteAnswer) return;
   const answer = JSON.parse(remoteAnswer);
-  if (!pc.currentRemoteDescription) {
-    pc.setRemoteDescription(answer);
+  if (!peerConnection.value.currentRemoteDescription) {
+    peerConnection.value.setRemoteDescription(answer);
   }
 };
 onMounted(() => {
@@ -128,6 +159,9 @@ onMounted(() => {
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+  &-main {
+    height: 100%;
   }
   &-footer {
     background-color: var(--el-color-primary-light-7);
